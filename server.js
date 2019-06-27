5b00d98dcafe4c18da1502a61662e7171819a0b5
Dan Correa <dancorrea3@gmail.com>
2019-06-27 18:10:01 -0400

    added mongoose require on server.js

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 require("dotenv").config();
 var express = require("express");
+var mongoose = require("mongoose");
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({
-  extended: false
+    extended: false
 }));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 app.use(express.json());
 app.use(express.static("routes/public"));
 
 require("./routes/htmlRoutes")(app);
 
+
+
 app.listen(PORT, function () {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
 });
 module.exports = app;
