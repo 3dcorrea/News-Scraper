106db07c84f38f995ae5a2a8b94f357487a9971d
Dan Correa <dancorrea3@gmail.com>
2019-06-27 18:50:16 -0400

    moved models folder into public

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var db = require("./models");
+var db = require("./routes/models");
 
 var PORT = 3000;
 
