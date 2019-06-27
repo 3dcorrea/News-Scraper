5d9e13adfc13c58a6ca7bc6d8db54442ffa2b65e
Dan Correa <dancorrea3@gmail.com>
2019-06-27 18:40:48 -0400

    added article.js, new server.js, index.js, note.js

--- server.js
+++ server.js
@@ -1,29 +1,92 @@
-require("dotenv").config();
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+app.use(logger("dev"));
 app.use(express.urlencoded({
-    extended: false
+    extended: true
 }));
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
-
 app.use(express.json());
-app.use(express.static("routes/public"));
+app.use(express.static("public"));
 
-require("./routes/htmlRoutes")(app);
+mongoose.connect("mongodb://localhost/unit18Populater", {
+    useNewUrlParser: true
+});
 
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.nationalenquirer.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        $("text-block promo-title").each(function (i, element) {
+            var result = {};
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
 
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({
+            _id: req.params.id
+        })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                note: dbNote._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 app.listen(PORT, function () {
-    console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-    );
+    console.log("App running on port " + PORT + "!");
 });
-module.exports = app;
